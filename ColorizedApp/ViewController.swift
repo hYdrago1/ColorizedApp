b48e33aa2869d1db6476641911aecc323068d88c
hYdrago1 <vanyagoncha@gmail.com>
2025-07-14 11:01:17 +0200

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Ivan Honcharov on 14.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
